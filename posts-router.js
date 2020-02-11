8b15b69482e3fac5cb2ce44ac9bd69e3c5f113eb
agohorel <gohorelalex@gmail.com>
2020-02-11 16:52:15 -0500

    add GET /posts/:id route

--- posts-router.js
+++ posts-router.js
@@ -64,4 +64,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await db.findById(id);
+
+    if (!post.length) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+
+    res.status(200).json(post);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
+});
+
 module.exports = router;
